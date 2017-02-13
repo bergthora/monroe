d077784102e8163b5774b1a5fba328b7aad1d304
eyglolilja <eyglolilja@gmail.com>
2017-02-13 14:27:32 +0000

    add slider

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ r.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=62361387d4ff155b91
 r.onreadystatechange = function () { 
   if (r.readyState != 4 || r.status != 200) return; // event sem segir þegar gögnin eru tilbúin á servernum. 
   const response = JSON.parse(r.responseText) //function sem breytir streng í json object form. 
-  const container = document.createElement("div"); //Bý til div sem heitir container. Ekki komin inná vefsíðuna ennþá. Er að vinna með þau.
+  const container = document.createElement("div");
+  container.id =  //Bý til div sem heitir container. Ekki komin inná vefsíðuna ennþá. Er að vinna með þau.
   const image = document.createElement("img");
   const title = document.createElement("h1");
   image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path  //til að fá þessa slóð. Fara inní Configuration á https://developers.themoviedb.org/3/configuration og í Try It Out flipann. Setja þar inní Api keyinn. 
   title.innerHTML = response.original_title; //original_title hér 
   container.append(image);
   container.append(title);
-  document.body.append(container);
+  //document.body.append(container);
 
 };
 r.send("banana=yellow");
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true
+  });
+});
+
+  
