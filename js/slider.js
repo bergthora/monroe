152aa4de26a0d6539a388dfda28d75145c4ee5fc
bergthora <bergthoraolafsdottir29@gmail.com>
2017-02-14 15:40:10 +0000

    update filtersida

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
     $('.superslider').slick({
     infinite: true,
-    slidesToShow: 1.1,
+    slidesToShow: 5.04,
     slidesToScroll: 5.0,
     arrows: true
+    responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 });
 
   
+
+$('#my-slider').show();
+$('#my-slider').foundation('_reflow');
