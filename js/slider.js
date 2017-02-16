af76dbf79d314fece592d72ad2e0c06b3782f5fb
bergthora <bergthoraolafsdottir29@gmail.com>
2017-02-16 01:47:49 +0000

    update

--- js/slider.js
+++ js/slider.js
@@ -25,14 +25,54 @@ $(document).ready(function(){
 
 $(function() {
     
-    $("#pips-slider")
+    $("#pips-slider1")
     
         .slider({
         
             range: true,
             min: 0,
             max: 10,
-            values: [ 4, 16 ],
+            values: [ 4, 10 ],
+        	step: 1
+        
+        })
+    
+        .slider("pips", {
+        	
+        	first: "label",
+        	last: "label",
+            rest: "label",
+        	step: 1,
+        	labels: false,
+        	prefix: "",
+        	suffix: ""
+        
+        })
+    
+    	.slider("float", {
+        	
+        	handle: true,
+        	pips: false,
+        	labels: false,
+        	prefix: "",
+        	suffix: ""
+        
+    	});
+
+
+});
+
+
+$(function() {
+    
+    $("#pips-slider2")
+    
+        .slider({
+        
+            range: true,
+            min: 1888,
+            max: 2017,
+            values: [ 1999, 2017 ],
         	step: 1
         
         })
