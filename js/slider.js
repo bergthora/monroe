5ce2656d3944046a39b592876a0a30ce2eb75cec
bergthora <bergthoraolafsdottir29@gmail.com>
2017-02-16 14:34:01 +0000

    updating slæder

--- js/slider.js
+++ js/slider.js
@@ -1,114 +1,11 @@
-$(document).ready(function(){
-    $('.superslider').slick({
-    infinite: true,
-    slidesToShow: 5.04,
-    slidesToScroll: 5.0,
-    arrows: true,
-    responsive: [
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1.1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-  });
-});
 
-  
-
-
-/*
-$(function() {
-    
-    $("#pips-slider1")
-    
-        .slider({
-        
-            range: true,
-            min: 0,
-            max: 10,
-            values: [ 4, 10 ],
-        	step: 1
-        
-        })
-    
-        .slider("pips", {
-        	
-        	first: "label",
-        	last: "label",
-            rest: "label",
-        	step: 1,
-        	labels: false,
-        	prefix: "",
-        	suffix: ""
-        
-        })
-    
-    	.slider("float", {
-        	
-        	handle: true,
-        	pips: false,
-        	labels: false,
-        	prefix: "",
-        	suffix: ""
-        
-    	});
-
-
-});
-
-
-$(function() {
-    
-    $("#pips-slider2")
-    
-        .slider({
-        
-            range: true,
-            min: 1888,
-            max: 2017,
-            values: [ 1999, 2017 ],
-        	step: 1
-        
-        })
-    
-        .slider("pips", {
-        	
-        	first: "label",
-        	last: "label",
-            rest: "label",
-        	step: 1,
-        	labels: false,
-        	prefix: "",
-        	suffix: ""
-        
-        })
-    
-    	.slider("float", {
-        	
-        	handle: true,
-        	pips: false,
-        	labels: false,
-        	prefix: "",
-        	suffix: ""
-        
-    	});
-
-
-});
-*/
-
+$(function(){
 $("#circles-slider")
    
    .slider({
        min: 0,
        max: 10,
-       values: [5, 10],
+       values: [8, 10],
        range: true,
        step: .5
    })
@@ -128,3 +25,5 @@ $(".slider")
                            
    
 });
+
+ 
